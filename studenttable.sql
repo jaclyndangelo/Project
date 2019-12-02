8c386c45abb10208ade3b4a2479136d2d0237fda
Jaclyn D'Angelo <54593484+jaclyndangelo@users.noreply.github.com>
2019-12-02 15:50:45 -0500

    Update studenttable.sql

--- studenttable.sql
+++ studenttable.sql
@@ -1,9 +1,9 @@
 CREATE TABLE students(
-	student_id int, 
+	student_id int UNIQUE PRIMARY KEY, 
 	first_name varchar,
 	last_name varchar,
 	address varchar,
 	date_of_birth date, 
-	social_security
-	sport_team_name
+	social_security int,
+	sport_team_name varchar
 );
