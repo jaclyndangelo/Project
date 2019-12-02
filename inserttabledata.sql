254c48d3f8430adf59f803c6608d86edea0a1436
Jaclyn D'Angelo <54593484+jaclyndangelo@users.noreply.github.com>
2019-12-02 16:26:13 -0500

    Update inserttabledata.sql

--- inserttabledata.sql
+++ inserttabledata.sql
@@ -1,15 +1,3 @@
-INSERT INTO students
-VALUES (0001, 'Hans', 'Deutsch', '6368 Limeport Pike', '1997-11-07', '123-45-6789', 2345, 321), 
-(0002, 'Braxton', 'Brent', NULL, '1997-12-22', '234-56-7890', 2345, 321), 
-(0003, 'Adam', 'Booz', '342 Souderton Lane', '345-67-8901',  2345, 321), 
-(0004, 'Logan', 'Barlock', '645 Parkland Ave', '142-12-2545', 2345, 321), 
-(0005, 'Dom', 'Johnson', '327 Pitcher Park', '176-34-4758', 2351, 321), 
-(0006, 'Nikki', 'Morollo', '4545 Green Way', '173-17-5362', 2349, 322),  
-(0007, 'Jaclyn', 'D'Angelo', '365 West Drive', '645-87-3746', 2350, 323), 
-(0008, 'Sarah', 'White', '4236 Ball Street', '475-23-5757', 2348, 324),
-(0009, 'Elainah', 'Boyles', '2545 Runners Lane', '237-45-8762', 2354, 325), 
-(0010, 'Grace', 'Gonzalas', '1234 Spring Street', 122-22-1234', 2352, 326)
-;
 
 INSERT INTO athletics
 VALUES (2345, 'Lacrosse', 'Men', 'Spring', 55, 15, 8, 3.0),
@@ -24,30 +12,45 @@ VALUES (2345, 'Lacrosse', 'Men', 'Spring', 55, 15, 8, 3.0),
 (2354, 'T&F', 'Womens', 'Fall', 50, 12, 7, 2.9)
 ;
 
+
+
+INSERT INTO math_major
+VALUES (10, 'Calc 1', 'Huddell', 'McInnis 254', 'Fall', 30, 3.6), 
+(11, 'Calc 2', 'McLallen', 'McInnis 243', 'Spring', 25, 3.3),
+(12, 'Calc 3', 'McLallen', 'McInnis 265', 'Fall', 20, 3.4), 
+(13, 'Advanced Calc', 'Huddell', 'Mcinnis 234', 'Spring', 15, 3.2),
+(14, 'Discrete', 'McLallen', 'Andrews 150', 'Spring', 17, 3.1),
+(15, 'Probability', 'McLallen', 'Andrews 150', 'Fall', 20, 3.6),
+(16, 'Abstract Algebra', 'McLallen', 'McInnis 254', 'Fall', 20, 3.0),
+(17, 'Number Theory', 'McLallen', 'McInnis 362', 'Spring', 18, 3.6),
+(18, 'Linear Algebra', 'Huddell', 'McInnis 312', 'Spring', 25, 3.7),
+(19, 'PDE', 'Huddell', 'Andrews 150', 'Fall', 15, 3.3)
+;
+
 INSERT INTO majors
 VALUES (321, 'Math', 11, 55, 2, 3.6),
 (322, 'Pyschology', 15, 100, 3, 3.3), 
 (333, 'Accounting', 14, 150, 4, 3.5), 
 (334, 'Education', 18, 250, 3, 3.7), 
 (335, 'Criminal Justice', 10, 75, 2, 3.4), 
-(335, 'Nursing', 18, 150, 5, 3.8), 
+(340, 'Nursing', 18, 150, 5, 3.8), 
 (336, 'Bible', 15, 50, 5, 3.6), 
 (337, 'Social Work', 13, 60, 2, 3.6), 
 (338, 'Athletic Training', 16, 80, 2, 3.5), 
 (339, 'Chemistry', 12, 35, 1, 3.7)
 ;
 
-INSERT INTO math_major
-VALUES (10, Calc 1, Huddell, McInnis 254, Fall, 30, 3.6), 
-(11, Calc 2, McLallen, McInnis 243, Spring, 25, 3.3),
-(12, Calc 3, McLallen, McInnis 265, Fall, 20, 3.4), 
-(13, Advanced Calc, Huddell, Mcinnis 234, Spring, 15, 3.2),
-(14, Discrete, McLallen, Andrews 150, Spring, 17, 3.1),
-(15, Probability, McLallen, Andrews 150, Fall, 20, 3.6),
-(16, Abstract Algebra, McLallen, McInnis 254, Fall, 20, 3.0),
-(17, Number Theory, McLallen, McInnis 362, Spring, 18, 3.6),
-(18, Linear Algebra, Huddell, McInnis 312, Spring, 25, 3.7),
-(19, PDE, Huddell, Andrews 150, Fall, 15, 3.3)
+INSERT INTO students
+VALUES (0001, 'Hans', 'Deutsch', '6368 Limeport Pike', '1997-11-07', '123456789', 2345, 321), 
+(0002, 'Braxton', 'Brent', NULL, '1997-12-22', '234567890', 2345, 321), 
+(0003, 'Adam', 'Booz', '342 Souderton Lane','1999-12-02', '345678901',  2345, 321), 
+(0004, 'Logan', 'Barlock', '645 Parkland Ave','1998-03-03', '142122545', 2345, 321), 
+(0005, 'Dom', 'Johnson', '327 Pitcher Park', '1997-05-16','176344758', 2351, 321), 
+(0006, 'Nikki', 'Morollo', '4545 Green Way', '1999-08-10','173175362', 2349, 322),  
+(0007, 'Jaclyn', 'DAngelo', '365 West Drive', '1998-09-08', '645873746', 2350, 321), 
+(0008, 'Sarah', 'White', '4236 Ball Street', '1997-08-18','475235757', 2348, 334),
+(0009, 'Elainah', 'Boyles', '2545 Runners Lane', '1998-04-19','237458762', 2354, 335), 
+(0010, 'Grace', 'Gonzalas', '1234 Spring Street', '1999-11-20','122221234', 2352, 336)
 ;
 
 INSERT INTO probability 
